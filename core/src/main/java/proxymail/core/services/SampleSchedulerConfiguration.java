c4a2ec7c9887b52ef59b73e38c9ac14c7425db5c
manda_kumar <manda.kumar@publicissapient.com>
2020-07-22 10:19:06 +0530

    committing the code

---
+++ core/src/main/java/proxymail/core/services/SampleSchedulerConfiguration.java
@@ -0,0 +1,35 @@
+package proxymail.core.services;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.AttributeType;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(
+        name = "Experience AEM: SlingSchedulerConfiguration",
+        description = "Sling scheduler configuration"
+)
+public  @interface SampleSchedulerConfiguration {
+	 @AttributeDefinition(
+	            name = "Scheduler name",
+	            description = "Name of the scheduler",
+	            type = AttributeType.STRING)
+	    public String schedulerName() default "Custom Sling Scheduler Configuration";
+
+	    @AttributeDefinition(
+	            name = "Enabled",
+	            description = "True, if scheduler service is enabled",
+	            type = AttributeType.BOOLEAN)
+	    public boolean enabled() default false;
+
+	    @AttributeDefinition(
+	            name = "Cron Expression",
+	            description = "Cron expression used by the scheduler",
+	            type = AttributeType.STRING)
+	    public String cronExpression() default "0/10 * * * * ?"; // runs every 10 seconds
+
+	    @AttributeDefinition(
+	            name = "Custom Path Parameter",
+	            description = "Custom path parameter to be used by the scheduler",
+	            type = AttributeType.STRING)
+	    public String customPathParameter() default "/content/dam";
+}
---
+++ core/src/main/java/proxymail/core/services/impl/NamingServiceImpl.java
@@ -0,0 +1,113 @@
+package proxymail.core.services.impl;
+
+import proxymail.core.services.*;
+import org.apache.felix.scr.annotations.*;
+import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.osgi.service.component.ComponentContext;
+import java.util.Map;
+
+@Component(immediate = true, metatype = true)
+@Service(NamingService.class)
+public class NamingServiceImpl implements NamingService{
+	private String name;
+	private String gender;
+	private boolean checkbox;
+	private String[] multiString;
+	int age;
+
+	/*
+	 * 
+	 * Name Constant Provides a consistent way to access the Felix's console
+	 * Author's name property also specifies, the default value of the variable
+	 */
+	@Property(label = "Name of the Author", value = "author")
+	private static final String AUTHOR_NAME = "author.value";
+	/*
+	 * 
+	 * Age Constant Provides a consistent way to access the Felix's console Author's
+	 * age property also specifies, the default value of the variable as integer
+	 * value
+	 */
+	@Property(label = "Age of the Author", intValue = 15)
+	private static final String AUTHOR_AGE = "author.age";
+
+	/*
+	 * Gender Constant with which to access the Felix's console Author's Gender
+	 * property also specified, default value of the variable as well as values for
+	 * options i.e. DropDown for Felix's console Property
+	 */
+	@Property(label = "Author Gender", description = "Describe Author Gender", options = {
+			@PropertyOption(name = "Male", value = "1. Male"),
+			@PropertyOption(name = "Female", value = "2. Female") }, value = "Female")
+
+	private static final String AUTHOR_GENDER = "author.gender";
+	@Property(label = "CheckBox Property ", boolValue = false, description = "please Check the Property")
+	public static final String CHECKBOX_PROPERTY = "checkBox.property";
+
+	@Property(value = { "English",
+			"Hindi" }, unbounded = PropertyUnbounded.ARRAY, label = "Subjects", cardinality = 50, description = "Example for Multi field config")
+	private static final String MULTI_FIELD = "multifield";
+
+	@Override
+    public String getAuthorName() {
+        return this.name;
+    }
+	
+	@Override
+	public String getAuthorGender() {
+		// TODO Auto-generated method stub
+		return this.gender;
+	}
+
+	@Override
+	public boolean getCheckbox() {
+		// TODO Auto-generated method stub
+		return this.checkbox;
+	}
+
+	@Override
+	public String[] getMultiString() {
+		// TODO Auto-generated method stub
+		return this.multiString;
+	}
+
+	@Override
+	public int getAge() {
+		// TODO Auto-generated method stub
+		return age;
+	}
+	@Activate
+	protected void activate(@SuppressWarnings("rawtypes") final Map context) {
+		this.name = PropertiesUtil.toString(context.get(AUTHOR_NAME), "");
+		this.gender = PropertiesUtil.toString(context.get(AUTHOR_GENDER), "");
+		this.checkbox = PropertiesUtil.toBoolean(context.get(CHECKBOX_PROPERTY), true);
+		this.multiString = PropertiesUtil.toStringArray(context.get(MULTI_FIELD));
+		this.age = PropertiesUtil.toInteger(context.get(AUTHOR_AGE), 12);
+
+	}
+
+	/*
+	 * sets variables to null when deactivating bundle
+	 */
+	@Deactivate
+	protected void deactivate() {
+		this.name = null;
+		this.gender = null;
+	}
+
+	/**
+	 * @param context assigns the values to variables when the values are modified
+	 *                in Felix's console
+	 */
+	@Modified
+	protected void modified(ComponentContext context) {
+		// gets values from context and assigns to instance variables
+		this.name = PropertiesUtil.toString(context.getProperties().get(AUTHOR_NAME), "");
+		this.gender = PropertiesUtil.toString(context.getProperties().get(AUTHOR_GENDER), "");
+		this.checkbox = PropertiesUtil.toBoolean(context.getProperties().get(CHECKBOX_PROPERTY), true);
+		this.multiString = PropertiesUtil.toStringArray(context.getProperties().get(MULTI_FIELD));
+		this.age = PropertiesUtil.toInteger(context.getProperties().get(AUTHOR_AGE), 12);
+	}
+
+	
+}
